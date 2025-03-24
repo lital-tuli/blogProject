e7149ae402d29b7a998b0e9c9efddb98acfdd2a6
lital-tuli <litaltuli@gmail.com>
2025-03-24 21:17:05 +0200

    Add user profile endpoint with authentication and serializer

--- django_blog_api/users/urls.py
+++ django_blog_api/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView
+from .views import RegisterView, get_user_profile
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', get_user_profile, name='user-profile'),
 ]
--- django_blog_api/users/views.py
+++ django_blog_api/users/views.py
@@ -1,11 +1,18 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User, Group
 from .serializers import UserSerializer
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user_profile(request):
+    user = request.user
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
 class RegisterView(APIView):
     permission_classes = [AllowAny]
     
