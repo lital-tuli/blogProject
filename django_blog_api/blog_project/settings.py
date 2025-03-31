3c50921c7c6ec1336a7d2f9cd901b4fdcd7fc41e
lital-tuli <litaltuli@gmail.com>
2025-03-31 15:58:51 +0300

    Enhance comment permissions and add reply functionality with improved validation

--- django_blog_api/blog_project/settings.py
+++ django_blog_api/blog_project/settings.py
@@ -83,13 +83,12 @@ DATABASES = {
 }
 
 # REST Framework settings
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',  
+        'rest_framework.permissions.AllowAny', 
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
--- django_blog_api/comments/views.py
+++ django_blog_api/comments/views.py
@@ -5,10 +5,17 @@ from django.shortcuts import get_object_or_404
 from articles.models import Article
 from .models import Comment
 from .serializers import CommentSerializer
+from core.permissions import IsAdminOrAuthorOrReadOnly
 
 class CommentViewSet(viewsets.ModelViewSet):
     """
     ViewSet for handling comment operations.
+    
+    Provides CRUD operations for comments with appropriate permissions:
+    - List/Retrieve: Available to all users
+    - Create: Limited to authenticated users
+    - Update: Limited to comment author
+    - Delete: Limited to admin users or comment author
     """
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
@@ -17,22 +24,42 @@ class CommentViewSet(viewsets.ModelViewSet):
         """
         Custom permissions based on action:
         - Anyone can view comments (list, retrieve)
-        - Authenticated users can create comments
-        - Admin users can delete any comment
-        - Users can only edit their own comments
+        - Authenticated users can create comments and replies
+        - Only admins or the comment author can delete a comment
+        - Only the author can update their own comments
         """
         if self.action in ['list', 'retrieve']:
             return [permissions.AllowAny()]
         elif self.action in ['create', 'reply']:
             return [permissions.IsAuthenticated()]
-        elif self.action == 'destroy':
-            return [permissions.IsAdminUser()]
-        else:
-            return [permissions.IsAuthenticated()]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            return [IsAdminOrAuthorOrReadOnly()]
+        return [permissions.IsAuthenticated()]
+    
+    def get_queryset(self):
+        """
+        Filters comments by article if article_pk is provided in the URL.
+        For list view, only return top-level comments (not replies).
+        """
+        queryset = Comment.objects.all()
+        
+        article_id = self.kwargs.get('article_pk')
+        if article_id:
+            queryset = queryset.filter(article__id=article_id)
+            
+        # If this is a list action, only return top-level comments
+        if self.action == 'list':
+            queryset = queryset.filter(reply_to__isnull=True)
+            
+        return queryset
     
     def create(self, request, *args, **kwargs):
         """
         Create a new comment for an article.
+        
+        URL: /api/articles/{article_pk}/comments/
+        Method: POST
+        Auth required: Yes
         """
         article_id = self.kwargs.get('article_pk')
         if not article_id:
@@ -50,32 +77,40 @@ class CommentViewSet(viewsets.ModelViewSet):
         reply_to_id = request.data.get('reply_to')
         if reply_to_id:
             reply_to = get_object_or_404(Comment, id=reply_to_id)
+            # Ensure reply is to a comment on the same article
+            if reply_to.article.id != article.id:
+                return Response(
+                    {"detail": "Reply must be to a comment on the same article."},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
             serializer.save(author=request.user, article=article, reply_to=reply_to)
         else:
             serializer.save(author=request.user, article=article)
             
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
-    def get_queryset(self):
+    def perform_create(self, serializer):
         """
-        Filters comments by article if article_pk is provided in the URL.
+        Sets the author to the current user when creating a comment.
+        This is only used when not overriding create() method.
         """
-        queryset = Comment.objects.all()
-        
-        article_id = self.kwargs.get('article_pk')
-        if article_id:
-            queryset = queryset.filter(article__id=article_id)
-            
-        # If this is a list action, only return top-level comments
-        if self.action == 'list':
-            queryset = queryset.filter(reply_to__isnull=True)
-            
-        return queryset
+        serializer.save(author=self.request.user)
+    
+    def perform_update(self, serializer):
+        """
+        Update a comment.
+        Only the author can update their own comment.
+        """
+        serializer.save()
     
     @action(detail=True, methods=['post'])
     def reply(self, request, pk=None):
         """
-        Create a reply to this comment.
+        Create a reply to an existing comment.
+        
+        URL: /api/comments/{pk}/reply/
+        Method: POST
+        Auth required: Yes
         """
         parent_comment = self.get_object()
         
