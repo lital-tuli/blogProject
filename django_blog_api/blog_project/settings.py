7edd85b8623057ffcfd174a9b1d3773f1e2e916f
lital-tuli <litaltuli@gmail.com>
2025-04-05 20:23:39 +0300

    Update ACCESS_TOKEN_LIFETIME to 1 hour for improved security

--- django_blog_api/blog_project/settings.py
+++ django_blog_api/blog_project/settings.py
@@ -104,7 +104,7 @@ REST_FRAMEWORK = {
 }
 # JWT settings
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),  
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
